ab7d7001a47f274e8aa86212d762a1fd25a4ecd3
digby-j <developer@jakedigby.com>
2022-05-18 17:03:55 -0400

    Added React Routing and User Content

---
+++ login.js
@@ -0,0 +1,7 @@
+function Login(){
+    const ctx = React.useContext(UserContext);
+    return (
+        <h1>Login<br/>
+            {JSON.stringify(ctx)}</h1>
+    );
+}
---
+++ withdraw.js
@@ -0,0 +1,7 @@
+function Withdraw(){
+    const ctx = React.useContext(UserContext);
+    return (
+        <h1>Withdraw<br/>
+            {JSON.stringify(ctx)}</h1>
+    );
+}
