c803606096c4f87755dd346db4330a57a833ee2a
digby-j <developer@jakedigby.com>
2022-05-20 13:37:28 -0400

    updated AllData to output test data.

--- alldata.js
+++ alldata.js
@@ -1,5 +1,10 @@
 function AllData(){
+    const ctx = React.useContext(userContext);
     return(
+        <>
         <h1>All Data Component</h1>
+        {JSON.stringify(ctx)}
+        </>
+
     );
 }
