9d6303c8ce3637dffd42cfb6ec8588169f56b91b
digby-j <developer@jakedigby.com>
2022-06-08 14:29:13 -0400

    fixed file struture and added relevent code

--- balance.js
+++
@@ -1,9 +0,0 @@
-function Balance(){
-    const ctx = React.useContext(UserContext);
-    return (
-        <>
-        <h1>Balance</h1>
-            {JSON.stringify(ctx)}<br/>
-        </>
-    );
-}
